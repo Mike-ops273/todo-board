11ed6048bcea126b523b620e3e328dc05484066e
mike-ops <mike@Mikes-MacBook-Air.local>
2023-03-14 16:01:54 +0200

    done dragdropcontext

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
+import "./App.css";
 
 function App() {
+  // state hooks
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
+  const [doneList, setDoneList] = useState([]);
+
+  // helper functions
+  const addTodo = () => {
+    if (!todo) {
+      alert("Enter a todo item");
+    }
+    const newTodo = {
+      id: Math.floor(Math.random() * 1000),
+      todo: todo,
+      isDone: false,
+    };
+    setTodoList((todoList) => [...todoList, newTodo]);
+    console.log(newTodo);
+    setTodo("");
+  };
+
+  // render
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>To Do Board</h1>
+        <div className="inputContainer">
+          <input
+            type="text"
+            placeholder="To do..."
+            name="todo"
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
+          />
+          <button onClick={() => addTodo()}>Add</button>
+        </div>
+        {todo}
       </header>
+      <DragDropContext>
+        <div className="todos-container">
+          <div className="board-column">
+            <h2>To Do</h2>
+            <div className="todo-todos">
+              <li>example</li>
+              <ul>
+                {todoList.map((todo) => {
+                  return (
+                    <li className="note" key={todo.id}>
+                      {todo.todo}
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </div>
+          <div className="board-column">
+            <h2>Done</h2>
+            <div className="done-todos">
+              <li>example</li>
+            </div>
+          </div>
+        </div>
+      </DragDropContext>
     </div>
   );
 }
